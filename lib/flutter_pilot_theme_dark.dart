87760fc0b85bb929de103cc799764d219bba84e2
Slarrauri <slarrauri@gmail.com>
2024-06-08 11:20:34 -0300

    init

---
+++ lib/flutter_pilot_theme_dark.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterPilotThemeDark {
+  static ThemeData get themeData {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+    );
+  }
+}
---
+++ lib/flutter_pilot_theme_light.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterPilotThemeLight {
+  static ThemeData get themeData {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+    );
+  }
+}
